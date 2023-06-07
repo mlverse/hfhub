da918d5961835782c4dccfcfb530839813c919f1
Daniel Falbel <dfalbel@gmail.com>
2023-06-07 11:56:08 +0200

    Add some tests

---
+++ tests/testthat/test-hub_download.R
@@ -0,0 +1,28 @@
+skip_on_cran()
+
+test_that("hub_download", {
+  file <- hub_download("gpt2", filename = "config.json")
+  expect_equal(
+    jsonlite::fromJSON(file)$architectures,
+    "GPT2LMHeadModel"
+  )
+
+  file <- hub_download("gpt2", filename = "config.json", force_download = TRUE)
+  expect_equal(
+    jsonlite::fromJSON(file)$architectures,
+    "GPT2LMHeadModel"
+  )
+
+  file <- hub_download("gpt2", filename = "config.json", local_files_only = TRUE)
+  expect_equal(
+    jsonlite::fromJSON(file)$architectures,
+    "GPT2LMHeadModel"
+  )
+
+  tmp <- tempfile()
+  dir.create(tmp)
+  withr::with_envvar(c(HUGGINGFACE_HUB_CACHE = tmp), {
+    file <- hub_download("gpt2", filename = "config.json")
+  })
+  expect_equal(list.files(tmp), "models--gpt2")
+})
